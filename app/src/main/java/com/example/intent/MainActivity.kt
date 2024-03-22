b2398417308462b962126ff5d816c7d12d99c5e8
SauloFernando99 <saulofernando114@gmail.com>
2024-03-22 18:47:21 -0300

    feat: implemented app's first version

--- app/src/main/java/com/example/intent/MainActivity.kt
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.intent
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import com.example.intent.Constantes.PARAMETRO_EXTRA
+import com.example.intent.Constantes.PARAMETRO_REQUEST_CODE
+import com.example.intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(amb.root)
+        amb.mainTb.apply {
+            title = getString(R.string.app_name)
+            subtitle = this@MainActivity.javaClass.simpleName
+            setSupportActionBar(this)
+        }
+        amb.entrarParametroBt.setOnClickListener{
+
+            //Intent IMPLICITA
+            Intent("ABRA_PARAMETRO_ACTIVITY").also {
+                it.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
+                startActivityForResult(it, PARAMETRO_REQUEST_CODE)
+            }
+
+            //INTENT EXPLICITA
+            /*            Intent(this, ParametroActivity::class.java).also {
+                            it.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
+                            startActivityForResult(it, PARAMETRO_REQUEST_CODE)
+                        }*/
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Constantes.PARAMETRO_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    val parametro = data.getStringExtra(Constantes.PARAMETRO_EXTRA)
+                    amb.parametroTv.text = parametro
+                }
+            }
         }
     }
 }
